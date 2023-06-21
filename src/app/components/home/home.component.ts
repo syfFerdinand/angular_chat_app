6bb5b5170cbe3557c3d57a6621ccc8c77a34db51
syfFerdinand <sambianiferdinand@gmail.com>
2023-06-21 11:00:11 +0000

    get current user logged data

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, HostListener } from '@angular/core';
 import { Router } from '@angular/router';
-import { User, users } from 'src/models/user.model';
+import { User } from 'src/models/user.model';
+import { UserService } from 'src/services/user/user.service';
 
 @Component({
   selector: 'app-home',
@@ -11,23 +12,29 @@ export class HomeComponent {
   currentUser: User | undefined
   windowHeight: string = '100vh'; // Hauteur initiale de la section
   listHeight: string = ''
+  user: User | undefined 
 
   constructor(
-    private router: Router
-  ){
-    
+    private router: Router,
+    private userService: UserService
+    ){  
     if(!localStorage.getItem('token'))
       router.navigate(['/login']);
   }
 
   ngOnInit(){
     this.updateWindowHeight();
-  }
+    this.userService.get().subscribe(
+      data=>{
+        this.user = data
+      }
+    )
+  } 
 
   onUserSelected(user: User) {
     this.currentUser = user;
   }
-
+  
   @HostListener('window:resize')
   onWindowResize() {
     this.updateWindowHeight();
